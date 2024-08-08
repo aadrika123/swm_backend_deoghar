0c8ef1a20089d0a3cca8d980e78b812be2206657
pandeyprity <pandeyprity33@gmail.com>
2024-08-08 14:06:47 +0530

    corrected code

--- app/Repository/ConsumerRepository.php
+++ app/Repository/ConsumerRepository.php
@@ -2245,7 +2245,7 @@ class ConsumerRepository implements iConsumerRepository
                 $val['remarks'] = $transaction->remarks;
                 $val['tcName'] = ($transaction->user_id) ? $this->GetUserDetails($transaction->user_id)->name : '';
                 // $val['verificationType'] = ($transaction->reconcilition_date) ? $verificationType : 'Pending';
-                $val['verificationType'] = $verificationType ?? 'Pending';
+                $val['verificationType'] = ($transaction->reconcilition_date) ? $verificationType : 'Pending';
                 $val['demandFrom'] = ($firstrecord) ? Carbon::create($firstrecord->payment_from)->format('d-m-Y') : '';
                 $val['demandUpto'] = ($firstrecord) ? Carbon::create($lastrecord->payment_to)->format('d-m-Y') : '';
                 $response[] = $val;
